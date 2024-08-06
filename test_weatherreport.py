de3a917a1497a014eeebd1410a6f8d9e231685f5
manojsubrahmanian <manoj.subramanian@gmail.com>
2024-08-06 18:23:17 +0530

    Tests corrected

--- test_weatherreport.py
+++ test_weatherreport.py
@@ -3,16 +3,16 @@ from weatherreport import report, WeatherSensorStub
 
 class TestWeatherReport(unittest.TestCase):
 
-    def test_rainy(self):
-        sensor_stub = WeatherSensorStub(5, 7, 3, 160)
+    def test_sunny_day(self):
+        sensor_stub = WeatherSensorStub(50, 10, 30, 55)
         weather_report = report(sensor_stub)
-        print(weather_report)
-        self.assertIn('rain', weather_report)
+        self.assertEqual(weather_report, 'Sunny day')
 
-    def test_high_precipitation_and_low_windspeed(self):
-        sensor_stub = WeatherSensorStub(50, 70, 30, 40)
+    def test_rainy_day(self):
+        sensor_stub = WeatherSensorStub(50, 70, 30, 55)
         weather_report = report(sensor_stub)
-        self.assertGreater(len(weather_report), 0)
+        self.assertEqual(weather_report, 'Alert: Stormy with heavy rain')
+        
 
 if __name__ == '__main__':
     unittest.main()
